a58c278d7cfb72dd47ff7275f2ac829508e034db
Umair-Abbas3512 <umairabbasyash@gmail.com>
2024-07-15 08:36:24 +0000

    container config donw

---
+++ Day01.js
@@ -0,0 +1,58 @@
+// Day 1: Variable and Data Types 
+// Task/Activities 
+
+
+// Activity  1: Varaible Declaration 
+// Task 1: Declare avarible using var, assign it a number, and log the value to the console.
+var a = 2;
+console.log(a);
+// Task 2: Declare a variable using let, assign it in a string, and log the value to the console.
+let b = 3;
+console.log(b);
+
+
+// Activity 2: Constant Derclaration 
+// Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+const c = true;
+console.log(c);
+
+
+// Activity 3: Data Types 
+// Task 4: Create variable of different data types (number, string, boolean, object, array) and log each variable,s type using the typeof operator.
+
+let str = "Hello";
+console.log(typeof str);
+
+let str1 = 2;
+console.log(typeof str1);
+
+let str3 = true;
+console.log(typeof str3);
+
+let str4 = ["apple", "Book"];
+console.log(typeof str4);
+
+const str5 = {
+    name: "Umair",
+    last: "Abbas"
+}
+console.log(typeof str5);
+
+
+
+// Activity 4: Reassigning Variable
+// Task 5: Declare a variable  using a variable using let, assignit an intial value, reassign a new value, and log both value to the console.
+let task5;
+task5 = "My name is Umair Abbas";
+console.log(task5);
+
+// Activity 5: Understanding const
+// Task 6: Try reassigning a variable declared with const and observed the error.
+// const task6;
+const task6 = 65;
+console.log(task6);
+
+
+
+
+
